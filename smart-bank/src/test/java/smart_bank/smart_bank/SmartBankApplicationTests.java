18fd719eb7407a1871f025b99518d01d778158a2
gulshensettarova <gulshensettarova@gmail.com>
2025-07-08 21:34:54 +0400

    Initial project

---
+++ smart-bank/src/test/java/smart_bank/smart_bank/SmartBankApplicationTests.java
@@ -0,0 +1,13 @@
+package smart_bank.smart_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
